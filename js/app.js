5468aa222ee5a5da054dfc1018052ab4cdcac419
Huw Fernie <HuwFernie@gmail.com>
2017-06-08 14:48:14 +0100

    basic flow sorted with empty functions

--- js/app.js
+++ js/app.js
@@ -4,6 +4,27 @@ poker.setup = function() {
 
 // run this on load!!
   console.log('Huw');
+  buildTheDeck();
 };
+
+function buildTheDeck(){
+  console.log('buildTheDeck');
+  shuffle();
+}
+
+function shuffle(){
+  console.log('shuffle');
+  deal();
+}
+
+function deal() {
+  console.log('dealing');
+  console.log('done');
+  whoWins();
+}
+
+function whoWins(){
+  console.log('Who wins');
+}
 
 $(poker.setup.bind(poker));
