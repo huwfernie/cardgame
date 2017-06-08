495592c19be1f67fb917f3b98a34793f31d478a4
Huw Fernie <HuwFernie@gmail.com>
2017-06-08 15:36:29 +0100

    working on the deal function

--- js/app.js
+++ js/app.js
@@ -15,14 +15,13 @@ poker.setup = function() {
 };
 
 function buildTheDeck(){
-  // builds an array of 52 objects, each object represents a card with keys of
-  // name, suit, value and image
-  // aces low
+  /* builds an array of 52 objects, each object represents a card with keys of
+  name, suit, value and image
+  aces low */
   console.log('buildTheDeck');
   const deck = [];
   const suits = ['hearts','diamonds','clubs','spades'];
   suits.forEach((suit) => {
-    console.log(suit);
     for(let i=1; i<=13; i++) {
       const thisCard = { name: i, suit, value: i, image: `${i}${suit[0]}` };
       deck.push(thisCard);
@@ -31,17 +30,40 @@ function buildTheDeck(){
   shuffle(deck);
 }
 
-function shuffle(deck){
-  console.log('shuffle');
-  // import fisher yates or similar
-  console.log(deck);
-  deal();
+
+function shuffle(array) {
+  /* This is a fisher-Yates shuffle algorithm, released under the
+  Apache License and taken from github via stack overflow:
+  https://github.com/coolaj86/knuth-shuffle
+  */
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  console.log('shuffled');
+  deal(array);
 }
 
-function deal() {
-  console.log('dealing');
-  console.log('done');
-  whoWins();
+function deal(deck) {
+  const $numberOfPlayers = $('#numberOfPlayers').val();
+  const $numberOfCards = $('#numberOfCards').val();
+  console.log(`Dealing ${$numberOfPlayers} players with ${$numberOfCards} cards`);
+  if(deck.length < ($numberOfCards * $numberOfPlayers)) {
+    console.log('numbers don\'t match');
+  } else {
+    console.log('done');    
+    whoWins();
+  }
 }
 
 function whoWins(){
